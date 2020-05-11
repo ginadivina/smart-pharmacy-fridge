75da26bdba7995485bb6518c55300ca3d0220237
Georgina <georginasoiland@gmail.com>
2020-05-11 12:35:45 +1000

    Graphs added to logging

--- app/routes.py
+++ app/routes.py
@@ -8,34 +8,36 @@ def index():
     data = get_medicine_stock()
     return render_template('base.html', medicine_stock_data=data)
 
+
 @app.route('/report')
 def report():
     data = get_sensors()
-    tempData = data['Temperature']
-    lowTempList = []
-    highTempList = []
+    temp_data = data['Temperature']
+    low_temp_list = []
+    high_temp_list = []
 
-    for index, reading in enumerate(tempData):
+    for index, reading in enumerate(temp_data):
         if reading[3] == "20":  #placeholder temperature for testing, change to: if tempData[i][3] < "2":
-            lowTempList.append(data['Temperature'][index])
+            low_temp_list.append(data['Temperature'][index])
         elif reading[3] == "20.1":  #placeholder temperature for testing, change to: if tempData[i][3] > "8":
-            highTempList.append(data['Temperature'][index])
+            high_temp_list.append(data['Temperature'][index])
 
+    temp_dict = {'Low Temperature Alerts': low_temp_list, 'High Temperature Alerts': high_temp_list}
+    return render_template('report.html', sensor_data=temp_dict)
 
-    tempDict = {'Low Temperature Alerts': lowTempList, 'High Temperature Alerts': highTempList}
-    return render_template('report.html', sensor_data=tempDict)
 
 @app.route('/sensors')
 def sensors():
     data = get_sensors()
     return render_template('sensors.html', sensor_data=data)
 
+
 @app.route('/api/sensor', methods=['POST'])
 def write_sensors():
-    sensordata = request.json
-    sensor_type_id = sensordata['sensor_type_id'] #placeholder to test, this variable is getting the data from the sensors
-    datetime = sensordata['datetime'] #placeholder to test
-    value = sensordata['value'] #placeholder to test
+    sensor_data = request.json
+    sensor_type_id = sensor_data['sensor_type_id'] #placeholder to test, this variable is getting the data from the sensors
+    datetime = sensor_data['datetime'] #placeholder to test
+    value = sensor_data['value'] #placeholder to test
 
     write_sensor_log(sensor_type_id, datetime, value)
     
